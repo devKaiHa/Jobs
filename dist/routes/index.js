812415410fc9e719dd812312a89b21ea95f79d49
DevZeroSh <m.shahrour.work@gmail.com>
2025-10-27 17:20:22 +0300

    fix: Add /dist to .gitignore to prevent build artifacts from being tracked

--- dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-const authRoute = require("./authRoute");
-const jobsCompaniesRoute = require("./jobs/jobsCompaniesRoute");
-const jobsUsersRoute = require("./jobs/jobsUsersRoute");
-const mountRoutes = (app) => {
-    app.use("/api/auth", authRoute);
-    app.use("/api/jobCompanies", jobsCompaniesRoute);
-    app.use("/api/jobUsers", jobsUsersRoute);
-};
-module.exports = mountRoutes;
--- dist/utils/tools/isEmail.js
+++
@@ -1,9 +0,0 @@
-//Email validation
-const isEmail = (email) => {
-    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-    if (!emailReg.test(email) || (email == ""))
-        return false;
-    else
-        return true;
-};
-module.exports = isEmail;
